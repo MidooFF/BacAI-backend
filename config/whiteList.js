306f49e3f26c85975b944c28cd782b959c881e67
tarek shno <mohammad.sh12121@gmail.com>
2026-02-14 14:46:15 +0300

    modified whiteList.js (again)

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,20 +1,8 @@
 const whiteList = [
   "http://localhost:5173",
   "https://bacai-sy.netlify.app",
-  "https://bacai-sy.netlify.app/",
-  "https://bacai-next.netlify.app/login",
-  "https://bacai-next.netlify.app/sign-up",
-  "https://bacai-next.netlify.app/forgot-password",
-  "https://bacai-next.netlify.app/reset-password",
-  "https://bacai-next.netlify.app/sign-up",
-  "https://bacai-next.netlify.app/verify-reset-code",
-  "https://bacai-next.netlify.app/services",
-  "https://bacai-next.netlify.app/services/arabic-writer",
-  "https://bacai-next.netlify.app/services/complex-num-trans",
-  "https://bacai-next.netlify.app/services/arabic-writer",
-  "https://bacai-next.netlify.app/services/consecutive-steady",
-  "https://bacai-next.netlify.app/services/arabic-writer",
-  "https://bacai-next.netlify.app/services/function-changes",
+  "https://bacai-next.netlify.app",
+  "https://bacai-backend.onrender.com",
   "http://localhost:3000",
 ];
 
